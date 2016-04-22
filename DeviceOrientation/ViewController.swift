028fba1409c2959acf0d8a5d9bfdb6d00bd5d75e
XDJwolf <1123098229@qq.com>
2016-04-22 16:25:24 +0800

    ios实战开发
    ios开发，swift版

--- DeviceOrientation/ViewController.swift
+++ DeviceOrientation/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orientationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "receivedRotation:", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        
+    }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        UIDevice.currentDevice().endGeneratingDeviceOrientationNotifications()
+        
+    }
+    func receivedRotation(notification:NSNotification)
+    {
+        let device = UIDevice.currentDevice()
+        switch device.orientation
+        {
+        case .Portrait:
+            self.orientationLabel.text = "竖直向上"
+        case .PortraitUpsideDown:
+            self.orientationLabel.text = "竖直向下"
+        case.LandscapeLeft:
+            self.orientationLabel.text = "水平向左"
+        case.LandscapeRight:
+            self.orientationLabel.text  = "水平向右"
+        case.FaceUp:
+            self.orientationLabel.text = "面朝上"
+        case.FaceDown:
+            self.orientationLabel.text = "面朝下"
+        case.Unknown:
+            self.orientationLabel.text = "未知"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
