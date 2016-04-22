9e3ed96822ac38217cdf8c8952d94f6deb325ef9
XDJwolf <1123098229@qq.com>
2016-04-22 11:56:39 +0800

    Initial Commit

---
+++ DeviceOrientation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceOrientation
+//
+//  Created by Apple on 16/4/22.
+//  Copyright © 2016年 xidian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
